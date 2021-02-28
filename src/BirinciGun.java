da08535ab9135509ea3c7f408fa29aa47211152e
yaseminozk <71727143+yaseminozk@users.noreply.github.com>
2021-02-28 18:31:11 -0500

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -4,5 +4,7 @@ public class BirinciGun {
 	public static void main(String[] args) {
 		System.out.println("11.57 Birinci Gun");
 		System.out.println("Ikinci commit");
+		System.out.println("10.11 Ikinci Gun");
+			
 	}
 }
