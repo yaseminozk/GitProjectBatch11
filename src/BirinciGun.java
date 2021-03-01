9e47917da835967f650207f418cbffa77e5316a6
yaseminozk <yaseminozkafa>
2021-02-28 20:27:40 -0500

    ikinci gun 2, commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -5,6 +5,6 @@ public class BirinciGun {
 		System.out.println("11.57 Birinci Gun");
 		System.out.println("Ikinci commit");
 		System.out.println("10.11 Ikinci Gun");
-			
+		System.out.println("10.26 Ikinci Gun");	
 	}
 }
